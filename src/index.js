e42104be4f93716b2c339861e9ffb189668ce191
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-04-09 14:35:10 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import {compose, pipe} from 'lodash/fp'
+
+let input = "      JavaScript      "
+let trim = str => str.trim()
+let toLowerCase = str => str.toLowerCase()
+// const wrapInDiv = str => `<div>${str}</div>`
+const wrap= type => str => `<${type}>${str}</${type}>`
+
+// const result = wrapInDiv(toLowerCase(trim(input)))
+
+
+const transform = pipe( trim, toLowerCase, wrap("span"), )
+
+console.log(transform(input))
+
+// console.log(result)
